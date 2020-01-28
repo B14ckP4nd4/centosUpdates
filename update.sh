5ebbba1c3f3e3bf13a19901b7b5ada039714afbb
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2020-01-28 14:53:46 +0100

    Update update.sh
    add " yum --disablerepo=\* --enablerepo=base,updates update " command for Centos 6.x

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-sudo yum update -y && sudo yum install epel-release -y && sudo yum group install "Development Tools" -y && sudo yum update -y && reboot now
+sudo yum --disablerepo=\* --enablerepo=base,updates update && sudo yum update -y && sudo yum install epel-release -y && sudo yum group install "Development Tools" -y && sudo yum update -y && reboot now
