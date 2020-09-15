ed82bc665230d43ae18c71674bcacacd6d006ed4
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2020-09-15 20:17:30 +0430

    Update update.sh
    add force yes to updater

--- update.sh
+++ update.sh
@@ -1,1 +1,6 @@
-sudo yum --disablerepo=\* --enablerepo=base,updates update && sudo yum update -y && sudo yum install epel-release -y && sudo yum group install "Development Tools" -y && sudo yum -y update && reboot now
+echo 'assumeyes=1' >> /etc/yum.conf &&
+sudo yum --disablerepo=\* --enablerepo=base,updates update &&
+sudo yes | yum update -y &&
+sudo yes | yum install epel-release -y &&
+sudo yes | yum group install "Development Tools" -y &&
+sudo yes | yum -y update && reboot now
