5344fe92f08eb48ae489f350353b1497299672c3
BlackPanda <46260610+B14ckP4nd4@users.noreply.github.com>
2021-06-21 01:42:23 -0700

    add some comments

--- update.sh
+++ update.sh
@@ -1,6 +1,18 @@
-echo 'assumeyes=1' >> /etc/yum.conf &&
-sudo yum --disablerepo=\* --enablerepo=base,updates update &&
-sudo yes | yum update -y &&
-sudo yes | yum install epel-release -y &&
-sudo yes | yum group install "Development Tools" -y &&
-sudo yes | yum -y update && reboot now
+#!/usr/bin/bash
+
+# set automatically yes for any update and install process
+echo 'assumeyes=1' >> /etc/yum.conf
+
+# update OS based on base repo
+sudo yum --disablerepo=\* --enablerepo=base,updates update
+yum update -y
+
+# install epel-release for access to epel packages
+yum install epel-release -y
+
+# install development tools like git and others
+yum group install "Development Tools" 
+
+#last update and reboot
+yum -y update
+reboot now
